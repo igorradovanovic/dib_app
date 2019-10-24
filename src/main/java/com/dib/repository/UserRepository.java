2c1e3e5c423ab2086f6ea9ce1e782ebaebe24b07
Igor.Radovanovic <igor.radovanovic@snt.rs>
2019-10-24 03:56:54 +0200

    ....

--- src/main/java/com/dib/repository/UserRepository.java
+++ src/main/java/com/dib/repository/UserRepository.java
@@ -13,22 +13,9 @@ import com.dib.sys.CustomRepository;
 @Repository
 public interface UserRepository extends CustomRepository<User, Long> {
 	 
-	User findByUserName(String name);
-	
-	User findByUserEmail(String email);
-	
-	User findByUserNameAndUserIdNot(String name, Long id);
-
-	User findByUserEmailAndUserIdNot(String email, Long id);
-	
 	User findOneByUserName(String name);
 	
 	@Query("SELECT r from UserRoles ur INNER JOIN ur.role r INNER JOIN ur.user u WHERE u.userName = :usrName")
     List<Role> findAuthorities(@Param("usrName") String username);
 	
-	@Query(value = "SELECT roles.rol_name FROM user_roles\r\n" + 
-			"Inner join roles\r\n" + 
-			"on  user_roles.urr_rol_id = roles.rol_id \r\n" + 
-			"where user_roles.urr_usr_id = :usrId", nativeQuery = true)
-	String getUserRole(@Param("usrId") Long usrId);
 }
